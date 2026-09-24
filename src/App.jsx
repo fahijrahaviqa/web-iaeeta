598ea01c2cd07177fb36faf4dc7f2ce77220de02
rizalwhyuu00 <rizal23ti@mahasiswa.pcr.ac.id>
2026-09-24 14:13:37 +0700

    memperbaiki halaman web site

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./pages/guest/Contact";
 const GuestLayout = React.lazy(() => import("./layout/GuestLayout"));
 const HeroSection = React.lazy(() => import("./components/HeroSection"));
 const About = React.lazy(() => import("./pages/guest/About"));
-const Login = React.lazy(() => import("./pages/auth/Login"));
+const Login = React.lazy(() => import("./pages/auth/Login")); 
 // const Register = React.lazy(() => import("./pages/auth/Register")); 
 const ResetPassword = React.lazy(() => import("./pages/auth/Reset"));
 const ForgotPassword = React.lazy(() => import("./pages/auth/Forgot"));
@@ -15,10 +15,9 @@ const Media = React.lazy(() => import("./pages/guest/Media"));
 const contact = React.lazy(() => import("./pages/guest/Contact"));
 const OurTim = React.lazy(() => import("./pages/guest/OurTim"));
 
-
 // --- PIKA ---
 const SidebarMenu = React.lazy(() => import("./components/SidebarMenu"));
-const Navbar = React.lazy(() => import("./components/Navbar"));
+const Navbar = React.lazy(() => import("./components/Navbar")); 
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 const Gallery = React.lazy(() => import("./pages/Gallery"));
 const Team = React.lazy(() => import("./pages/Team"));
@@ -26,6 +25,7 @@ const Settings = React.lazy(() => import("./pages/Settings"));
 
 // --- Komponen Loading ---
 const Loading = React.lazy(() => import("./components/Loading"));
+
 // Layout halaman admin
 function AdminLayout() {
   return (
@@ -41,24 +41,32 @@ function AdminLayout() {
               <Route path="tim-kami" element={<Team />} />
               <Route path="pengaturan" element={<Settings />} />
             </Routes>
-
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route element={<GuestLayout />}>
-                <Route path="/" element={<HeroSection />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/OurTim" element={<OurTim />} />
-                <Route path="/Media" element={<Media />} />
-                <Route path="/Contact" element={<Contact />} />
-              </Route>
-              <Route path="/admin/*" element={<AdminLayout />} />
-            </Routes>
           </Suspense>
         </main>
       </div>
     </div>
+  );
+}
+
+
+export default function App(){
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route element={<GuestLayout />}>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/OurTim" element={<OurTim />} />
+          <Route path="/Media" element={<Media />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Route>
+        <Route path="/admin/*" element={<AdminLayout />} />
+      </Routes>
+    </Suspense>
   );
 }
